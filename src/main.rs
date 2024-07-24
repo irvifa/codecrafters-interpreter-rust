b80bdbf65be25365369885552c1bfde90f328def
Irvi Aini <irvi.fa@gmail.com>
2024-07-24 16:10:56 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,6 @@ fn main() {
 
     match command.as_str() {
         "tokenize" => {
-            // You can use print statements as follows for debugging, they'll be visible when running tests.
             writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
 
             let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
@@ -22,16 +21,61 @@ fn main() {
                 String::new()
             });
 
-            // Uncomment this block to pass the first stage
-            if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
-            } else {
-                println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
+            let mut scanner = Scanner::new(&file_contents);
+            let tokens = scanner.scan_tokens();
+
+            for token in tokens {
+                println!("{}", token);
             }
         }
         _ => {
             writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
-            return;
         }
+    }
+}
+
+#[derive(Debug)]
+enum TokenType {
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    EOF,
+}
+
+impl std::fmt::Display for TokenType {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            TokenType::LEFT_PAREN => write!(f, "LEFT_PAREN ( null"),
+            TokenType::RIGHT_PAREN => write!(f, "RIGHT_PAREN ) null"),
+            TokenType::EOF => write!(f, "EOF  null"),
+        }
+    }
+}
+
+struct Scanner<'a> {
+    source: &'a str,
+    tokens: Vec<TokenType>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(source: &'a str) -> Self {
+        Scanner {
+            source,
+            tokens: Vec::new(),
+        }
+    }
+
+    fn scan_tokens(&mut self) -> &Vec<TokenType> {
+        let chars: Vec<char> = self.source.chars().collect();
+
+        for &c in &chars {
+            match c {
+                '(' => self.tokens.push(TokenType::LEFT_PAREN),
+                ')' => self.tokens.push(TokenType::RIGHT_PAREN),
+                _ => {}
+            }
+        }
+
+        self.tokens.push(TokenType::EOF);
+        &self.tokens
     }
 }
